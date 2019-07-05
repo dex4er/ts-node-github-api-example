b08d0f245c0c4ce62806a2db408b19a3944093e8
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2019-07-05 10:13:22 +0200

    Use offline version of API schema

--- bin/github-apiv4-get-schema.ts
+++ bin/github-apiv4-get-schema.ts
@@ -2,17 +2,21 @@
 
 /// <reference types="node" />
 
+import JSONstringify from "fast-safe-stringify"
 import fs from "fs"
 import fetch from "node-fetch"
 
 const GITHUB_API_URL = "https://api.github.com/graphql"
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN
-const SCHEMA_JSON = "schema/index.json"
+const SCHEMA_JSON = "schema/json/schema.json"
 
-fetch(GITHUB_API_URL, {headers: {Authorization: "bearer " + GITHUB_TOKEN}})
-  .then(res => {
-    const schemaFile = fs.createWriteStream(SCHEMA_JSON)
-    res.body.pipe(schemaFile)
-    console.info({[SCHEMA_JSON]: {[GITHUB_API_URL]: res.statusText}})
+async function main(): Promise<void> {
+  const res = await fetch(GITHUB_API_URL, {
+    headers: {Authorization: "bearer " + GITHUB_TOKEN},
   })
-  .catch(console.error)
+  const data = await res.json()
+  fs.writeFileSync(SCHEMA_JSON, JSONstringify(data.data, undefined, 2))
+  console.info({[SCHEMA_JSON]: {[GITHUB_API_URL]: res.statusText}})
+}
+
+void main().catch(console.error)
