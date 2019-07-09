b16f4a9a80492fb79100daca405cc75fa9cd3988
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2019-07-09 18:55:12 +0200

    pipeline: git diff before and after clean

--- .gitlab-ci/helpers/git-diff.sh
+++ .gitlab-ci/helpers/git-diff.sh
@@ -1,5 +1,5 @@
 git diff --stat --exit-code
 
-for f in $(git ls-files --others --exclude-standard); do
+for f in $(git ls-files --others ${GIT_LS_FILES_EXCLUDE_ARGS:---exclude-standard}); do
     git diff --no-index --stat --exit-code /dev/null $f
 done
--- .gitlab-ci/test-app.sh
+++ .gitlab-ci/test-app.sh
@@ -3,13 +3,17 @@ rm -rf node_modules/*
 
 yarn install --non-interactive
 
+. .gitlab-ci/helpers/test-audit.sh
+
 npm pack
 
 yarn lint
 yarn test:script
 
-. .gitlab-ci/helpers/test-audit.sh
+. .gitlab-ci/helpers/git-diff.sh
 
 yarn clean
+
+GIT_LS_FILES_EXCLUDE_ARGS="-x .cache -x .install -x .nyc_output -x .packages -x *.tgz -x /coverage -x /node_modules -x yarn.lock -x yarn-error.log"
 
 . .gitlab-ci/helpers/git-diff.sh
